6469bb25cf2b1f9e920f4df72846e498b29bda9b
NoHaxJustTuL <Thanachot.tul@gmail.com>
2026-01-21 00:04:41 +0700

    first commit

---
+++ 99prelab/MyFirstProject/MyFirstProject.srcs/sources_1/new/blinky.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/16/2026 07:46:57 PM
+// Design Name: 
+// Module Name: blinky
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module blinky(
+    input clk,
+    output led
+    );
+    reg [24:0] count = 0;
+ 
+    assign led = count[24];
+ 
+    always @ (posedge(clk)) count <= count + 1;
+endmodule
